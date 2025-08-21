a1ffcf0efe50d3d07c8cfb2592d140990c55555e
GhyunOh <dhwlgus3456@naver.com>
2025-08-21 18:16:31 -0500

    Rmove telescope-log files except Readme

--- telscope-log/AWS_dataset/unstructured/preprocessing.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python preprocessing.py attack.csv AWS_abnormal_log.csv
-python preprocessing.py normal.csv AWS_normal_log.csv
--- telscope-log/AWS_dataset/unstructured/run.sh
+++
@@ -1,4 +0,0 @@
-
-bash preprocessing.sh
-python from_token_to_index.py
-python gen_train_data.py
